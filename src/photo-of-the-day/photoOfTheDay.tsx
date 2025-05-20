d3587d8f89e906349e813d8f3d8d1bf337a1430f
Simon Duck <its.duck@gmail.com>
2025-05-20 12:29:43 +0100

    minor CSS to main image

--- src/photo-of-the-day/photoOfTheDay.tsx
+++ src/photo-of-the-day/photoOfTheDay.tsx
@@ -1,29 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { data } from "react-router";
+import "./photoOfTheDay.scss";
 
-type PhotoOfTheDay = {
-    hdurl : string;
-}
 
 export default function PhotoOfTheDay () {
 
     const [photo, setPhoto] = useState<string>()
+    const [title, setTitle] = useState<string>()
+    const [explanation,setExplanation] = useState<string>()
 
     useEffect (() => {
         fetch("https://api.nasa.gov/planetary/apod?api_key=5s0C1UhCZLh3WVVN2kGzF1kEw76CImpsBBf3AvEy")
         .then(response => 
             response.json())
-            .then(data => {setPhoto(data.hdurl)
+            .then(data => {setPhoto(data.hdurl) 
+                setTitle(data.title)
+                setExplanation(data.explanation)
                  console.log(data)
+
             //setMyData(data.results)
             })
     .catch(error => console.error("error", error))}, []);
+     const photoData= data
 
-
-return ( 
-        <div>
-            <img src={photo} width="100%" />
+return ( <div>
+        <div className="photoOfTheDayContainer">
+            <img src={photo} className="photo-of-the-day" width="100%" alt-text={title} />
+            
         </div>
-
+<div>
+        {explanation}
+        </div>
+</div>
 )
 
 }
