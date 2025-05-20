cd8f7ece40626cfa4ff8a3ccd36f2c20cb61f18c
Sathya Sreenivasan <sathya.sreenivasan@softwire.com>
2025-05-20 15:03:52 +0100

    display image only if url is present in the response

--- src/photo-of-the-day/photoOfTheDay.tsx
+++ src/photo-of-the-day/photoOfTheDay.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { data } from "react-router";
 import "./photoOfTheDay.scss";
+//import dayjs from 'dayjs';
 
 
 export default function PhotoOfTheDay () {
@@ -8,6 +9,22 @@ export default function PhotoOfTheDay () {
     const [photo, setPhoto] = useState<string>()
     const [title, setTitle] = useState<string>()
     const [explanation,setExplanation] = useState<string>()
+
+
+    // let day = dayjs().date()
+    // let month = dayjs().month()+1
+    // let year= dayjs().year()
+
+    // console.log (year & month & day)
+
+    // date={year}-{month}-{day}
+
+
+    // if data.mediatype != "image" && day > 1
+    // {
+    //     day -=1
+    // }
+
 
     useEffect (() => {
         fetch("https://api.nasa.gov/planetary/apod?api_key=5s0C1UhCZLh3WVVN2kGzF1kEw76CImpsBBf3AvEy")
@@ -24,11 +41,13 @@ export default function PhotoOfTheDay () {
      const photoData= data
 
 return ( 
-<div>
+<div>   
         <div className="photoOfTheDayContainer">
-            <img src={photo} className="photo-of-the-day" width="100%" alt-text={title} />
-            
-        </div>
+        {photo ? (
+            <img src={photo} className="photo-of-the-day" alt-text={title} />
+            )
+     : (<p> Display Video </p>)}
+    </div>
         <div className="explanation">
         {explanation}
         </div>
