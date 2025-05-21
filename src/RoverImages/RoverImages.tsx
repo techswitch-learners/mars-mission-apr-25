6ff8f42f0abfbedabce2ad241f44df3f2c414631
preek0719 <preetha.krishnan@softwire.com>
2025-05-21 11:38:29 +0100

    added loading, error messaging, cleaned up api fetch calls

--- src/RoverImages/RoverImages.tsx
+++ src/RoverImages/RoverImages.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Carousel } from "react-responsive-carousel";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './RoverImages.scss';
+import { RotatingLines } from "react-loader-spinner";
 
 type RoverResponse = {
     id: number,
@@ -11,59 +14,79 @@ type RoverResponse = {
 // const api = 'ffiSxDvu6k6PrMaGjIkoHGXNBaZMWFwqmOBIe2cZ'
 const api = 'fCp5fNsscdDmov0Vw4lpU4bOkdMTCuCA9tnoKgYH'
 
-function RoverImages(props: {name: string}) {
+function RoverImages(props: { name: string }) {
     const [roverResponse, setRoverResponse] = useState<RoverResponse[]>();
-    const [latestPhotoDate, setLatestPhotoDate] = useState<string>();
+    const [latestPhotoDate, setLatestPhotoDate] = useState<string>("");
     const [latestCameras, setLatestCameras] = useState<[]>();
-    const [isLoading, setLoading] = useState<boolean>(true);
+    const [isLoading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>("");
+
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
 
     useEffect(() => {
+    const fetchData = async () => {
         try {
-        fetch (`https://api.nasa.gov/mars-photos/api/v1/manifests/${props.name}?api_key=${api}`)
-        .then(response => response.json())
-        
-        .then(data => {
-            setLatestPhotoDate(data.photo_manifest.max_date)
-            setLatestCameras(data.photo_manifest.photos[data.photo_manifest.photos.length -1].cameras)
-        })
-    }
-    catch (err){
-       console.log(err);
-    }
-    console.log(latestPhotoDate)
-}, [])
+        setLoading(true)    
+        const manifestResponse = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${props.name}?api_key=${api}`);
+        const manifestData = await manifestResponse.json();
+        console.log(manifestData)
+        setLatestPhotoDate(manifestData.photo_manifest.max_date);
+        setLatestCameras(manifestData.photo_manifest.photos[manifestData.photo_manifest.photos.length - 1].cameras);
 
+        const photoResponse = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${props.name}/photos?earth_date=${manifestData.photo_manifest.max_date}&api_key=${api}`);
+        const photoData = await photoResponse.json();
 
-      useEffect(() => {
-        if(latestPhotoDate){
-      try {
-        fetch (`https://api.nasa.gov/mars-photos/api/v1/rovers/${props.name}/photos?earth_date=${latestPhotoDate}&api_key=${api}`)
-        .then(response => response.json())
-        .then(data => {
-            setRoverResponse(data.photos)
-            setLoading(false)
-        })
-    }
-    catch (err){
-       console.log(err);
-    }
-    } }, [latestPhotoDate])
+        setRoverResponse(photoData.photos);
+        setLoading(false);
+            } catch (error) {
+                setLoading(false);
+                console.error(error);
+                setError(error instanceof Error ? error.message : "An unknown error occurred.");
+            }
+        };
 
-    return (
-        //this needs to display loading first then nothing to display otherwise 
-        <ul>
-            {!roverResponse ? (
-            <h2> Loading...!</h2>
-            ): 
-            ( roverResponse.map((image) => (
-                <li>
-                    <img key= {image.id} src={image.img_src} />
-                </li>    
-            ))
-        )   
-        }
-       </ul>
-    )
+  fetchData();
+}, [props.name]); 
+    
+
+    if (error) {
+        return (
+            <div>
+                <p className="error">Sorry, there's been an error with the page, please try again later!</p>
+            </div>
+        )
+    }
+    if (isLoading == true && !roverResponse) {
+        return (
+           <div className="sliderContainer">
+                <RotatingLines
+                    strokeColor="grey"
+                    strokeWidth="5"
+                    animationDuration="0.75"
+                    width="96"
+                    visible={true}
+                />
+            </div>
+        )
+    }
+    else if (roverResponse) {
+        return (
+            <div className="sliderContainer">
+                <Slider {...settings}>
+                {roverResponse.map((image) => (
+                    <img key={image.id} src={image.img_src} />
+                ))}
+                </Slider>
+            </div>
+            
+        )
+    }
 }
 
 export default RoverImages;
