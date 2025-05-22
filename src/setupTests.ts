c35e86011d4bf1a776528a0ad7a023454ff7c90e
Holly Knox <hollyknox15@gmail.com>
2025-05-22 14:58:43 +0100

    Rover image component (#23)
    Co-authored-by: preek0719 <preetha.krishnan@softwire.com>
    Co-authored-by: Rachel Kelly <rachel.kelly@softwire.com>

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,11 @@
 import "@testing-library/jest-dom";
-import { TextEncoder } from "node:util";
 
-if (!global.TextEncoder) {
-  global.TextEncoder = TextEncoder;
-}
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
