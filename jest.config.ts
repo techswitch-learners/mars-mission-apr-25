eb44fdc3102efebc37c64f8c75f15276eb4fdc7b
Simon Duck <its.duck@gmail.com>
2025-05-22 14:27:41 +0100

    removed commented config

--- jest.config.ts
+++ jest.config.ts
@@ -3,16 +3,3 @@ const config = {
 };
 
 export default config;
-
-// export default {
-//   transform: {
-//     ".(ts|tsx)": "ts-jest",
-//   },
-//   moduleNameMapper: {
-//     ".+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$":
-//       "identity-obj-proxy",
-//   },
-//   moduleFileExtensions: ["ts", "tsx", "js", "json"],
-//   testEnvironment: "jsdom",
-//   setupFilesAfterEnv: ["@testing-library/jest-dom"],
-// };
