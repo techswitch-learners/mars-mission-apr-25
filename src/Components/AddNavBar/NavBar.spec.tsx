ba630011d70e535389615b6a176d2aa7efdc49b7
Eleanor <eleanor.scott@softwire.com>
2025-05-22 18:36:04 +0100

    Fix test import

--- src/Components/AddNavBar/NavBar.spec.tsx
+++ src/Components/AddNavBar/NavBar.spec.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import NavBar from "./NavBar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 
 describe(NavBar, () => {
   it("Should change current location to profile when link is clicked", () => {
