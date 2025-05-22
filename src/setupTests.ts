4f2f59de23b4536a6cec84000a1d6ac31e352c51
hk-15 <hollyknox15@gmail.com>
2025-05-22 13:56:50 +0100

    Fix issues raised in PR

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,4 @@
 import "@testing-library/jest-dom";
-// import { TextEncoder } from "node:util";
-
-// if (!global.TextEncoder) {
-//   global.TextEncoder = TextEncoder;
-// }
 
 global.matchMedia =
   global.matchMedia ||
