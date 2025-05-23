7c3c9a4d0f1c785d15841c2320e04bede2d1b7cb
Simon Duck <its.duck@gmail.com>
2025-05-23 10:26:47 +0100

    Add nav bar (#25)
    Co-authored-by: Sathya Sreenivasan <sathya.sreenivasan@softwire.com>
    Co-authored-by: Eleanor <eleanor.scott@softwire.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,30 @@ import "./App.scss";
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router";
 import PhotoOfTheDay from "./Components/photoOfTheDay/photoOfTheDay";
 import ProfilePage from "./pages/Profile/ProfilePage";
+import NavBar from "./Components/AddNavBar/NavBar";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PhotoOfTheDay />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route
-          path="*"
-          element={
-            <div>
-              Sorry, that page does not exist, try these:
+    <div>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<PhotoOfTheDay />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route
+            path="*"
+            element={
               <div>
-                <Link to="/profile">Profile Page</Link>
+                Sorry, that page does not exist, try these:
+                <div>
+                  <Link to="/profile">Profile Page</Link>
+                </div>
               </div>
-            </div>
-          }
-        />
-      </Routes>
-    </Router>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
