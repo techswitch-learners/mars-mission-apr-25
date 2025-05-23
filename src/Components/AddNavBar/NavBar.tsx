730514a2f0c7cbc53df256de30f21de2fb6af92c
hk-15 <hollyknox15@gmail.com>
2025-05-23 11:19:25 +0100

    Run lint fix

--- src/Components/AddNavBar/NavBar.tsx
+++ src/Components/AddNavBar/NavBar.tsx
@@ -7,26 +7,26 @@ import imageEarth from "./Images/cartoon earth.png";
 export default function NavBar() {
   return (
     <div>
-        <header className="navBarHeader">
-          <nav className="navBarContainer">
-            <div>
-              <Link to="/" className="homeButton" id="homeLink">
-                <img src={imageEarth} alt="imageEarth" id="earthButton" />
-                Home
-              </Link>
-            </div>
-            <div>
-              <Link to="/profile" className="profilePageButton" id="profileLink">
-                <img
-                  src={imageAstro}
-                  alt="cartoon astronaut illustration"
-                  id="astroButton"
-                />
-                Profile
-              </Link>
-            </div>
-          </nav>
-        </header>
+      <header className="navBarHeader">
+        <nav className="navBarContainer">
+          <div>
+            <Link to="/" className="homeButton" id="homeLink">
+              <img src={imageEarth} alt="imageEarth" id="earthButton" />
+              Home
+            </Link>
+          </div>
+          <div>
+            <Link to="/profile" className="profilePageButton" id="profileLink">
+              <img
+                src={imageAstro}
+                alt="cartoon astronaut illustration"
+                id="astroButton"
+              />
+              Profile
+            </Link>
+          </div>
+        </nav>
+      </header>
       <div className="stars"></div>
     </div>
   );
